fba406bc81c403d699c173d5d36c1ec13c430bb0
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-25 19:41:27 -0300

    Finished modules lesson and preparing to enter Mix

--- elixir_school/modules.exs
+++ elixir_school/modules.exs
@@ -28,3 +28,38 @@ defmodule ExampleThree do
 end
 
 IO.puts ExampleThree.greeting("Andressa")
+
+defmodule Sayings.Greetings do
+  def basic(name), do: "Hi, #{name}"
+end
+
+defmodule Example do
+  alias Sayings.Greetings
+
+  def greeting(name), do: Greetings.basic(name)
+end
+
+IO.puts Example.greeting("Arthur")
+
+import List
+IO.puts last([1, 2, 3])
+
+import List, only: [last: 1]
+
+# first([1, 2, 3]) This will not work
+
+defmodule Hello do
+  defmacro __using__(opts) do
+    greeting = Keyword.get(opts, :greeting, "Hi")
+
+    quote do
+      def hello(name), do: unquote(greeting) <> ", " <> name
+    end
+  end
+end
+
+defmodule Example do 
+  use Hello, greeting: "Hola"
+end
+
+IO.puts Example.hello("Franco")
